0de88d5824cd21d977153b55de228f036c48be39
Michael Banzeu <banzeumike@gmail.com>
2023-07-23 14:17:21 +0100

    Update script.js
    Update of the api key

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const api = {
-    key: "afaf9f8d48cff6cafd32e23220bcfdbf",
-    baseUrl: "https://api.openweatherapp.org/data/2.5/"
+    key: "080eac3f308aaedd1d049af637ee543a",
+    baseUrl: "https://api.openweathermap.org/data/2.5/"
 }
 
 const searchBox = document.querySelector('.search-box');
@@ -12,6 +12,8 @@ function setQuery(event) {
     }
 }
 
+window.addEventListener('load', getResults("Douala"));
+
 function getResults(query) {
     fetch(`${api.baseUrl}weather?q=${query}&units=metric&APPID=${api.key}`)
         .then(weather => {
@@ -20,7 +22,6 @@ function getResults(query) {
 }
 
 function displayResults(weather) {
-    // console.log(weather);
     let city = document.querySelector('.location .city');
     city.innerText = `${weather.name}, ${weather.sys.country}`;
 
@@ -29,7 +30,7 @@ function displayResults(weather) {
     date.innerText = dateBuilder(now);
 
     let temp = document.querySelector('.current .temp');
-    temp.innerText = `${Math.round(weather.main.temp)}<span>°C</span>`;
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
 
     let weather_el = document.querySelector('.current .weather');
     weather_el.innerText = weather.weather[0].main;
@@ -40,7 +41,7 @@ function displayResults(weather) {
 
 function dateBuilder(d) {
     let months = ["January", "Febraury", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Saturday"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
     let day = days[d.getDay()];
     let date = d.getDate();
